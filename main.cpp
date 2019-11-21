01783a696767577c8bd89c1c593b99dab2647f05
ClementC <clem.calliau@hotmail.fr>
2019-11-21 14:23:53 +0100

    add localhost adress

--- main.cpp
+++ main.cpp
@@ -19,6 +19,7 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+//    QString address = "192.168.0.51";
     QString address = "localhost";
     quint16 port = 1883;
     QList<QString> topicList;
