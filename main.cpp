833021b84bcc5b71c2155cc8c6b019ad2a6cec17
Joris Offouga <offougajoris@gmail.com>
2019-11-21 14:21:08 +0100

    Update mqttHandler
    Add destructor
    Add publishdata to json
    move m_client to public
    
    Signed-off-by: Joris Offouga <offougajoris@gmail.com>

--- main.cpp
+++ main.cpp
@@ -9,8 +9,28 @@ public:
 
 public slots:
     void onMessage(QMqttMessage message) override{
+        quint32 rc;
+        QString topic = "/data";
+        QJsonObject jobject;
+
         qDebug() << "mqtt message receive fils from topic : " <<  message.topic().name() << " payload : " <<
                     message.payload();
+
+        if(message.topic().name() == "/test"){
+            jobject["Temperature"] = QString::number(25);
+
+            jobject["Type"] = "Data";
+
+            this->publishData(topic, jobject);
+        }else if(message.topic().name() == "/stuff"){
+            QJsonDocument dataJSON = QJsonDocument::fromJson(message.payload());
+            QJsonObject data = dataJSON.object();
+            int value1 = data["value1"].toInt();
+            int value2 = data["value2"].toInt();
+
+            qDebug() << value1;
+            qDebug() << value2;
+        }
     }
 };
 
