65a997a5ab232756fba8250b230553c186805885
ClementC <clem.calliau@hotmail.fr>
2019-11-22 10:41:51 +0100

    sub & pub __ tvoc & co2 ok

--- mqttherit.cpp
+++ mqttherit.cpp
@@ -5,21 +5,20 @@ MqttHerit::MqttHerit(QString address, quint16 port, QList<QString> topicList): M
 
 
 void MqttHerit::onMessage(QMqttMessage message) {
-    quint32 rc;
+//    quint32 rc;
     QJsonObject jobject;
-    QString topic = "bla";
+    QString topic = "data";
 
    qDebug() << "mqtt message receive fils from topic : " <<  message.topic().name() << " payload : " << message.payload();
 
    if(message.topic().name() == "/test"){
-       topic = "/data";
+       jobject["data"] = "test_data";
    }else if(message.topic().name() == "/sensor/tvoc"){
-       topic = "/sensor/tvoc";
-       this->publishData(topic, jobject);
+       jobject["data"] = "test_tvoc";
    }else if(message.topic().name() == "/sensor/co2"){
-       topic = "/sensor/co2";
-       this->publishData(topic, jobject);
+       jobject["data"] = "test_co2";
    }else if(message.topic().name() == "/stuff"){
+       topic = message.topic().name();
        QJsonDocument dataJSON = QJsonDocument::fromJson(message.payload());
        QJsonObject data = dataJSON.object();
        int value1 = data["value1"].toInt();
@@ -27,10 +26,11 @@ void MqttHerit::onMessage(QMqttMessage message) {
 
        qDebug() << value1;
        qDebug() << value2;
+   }else{
+       qDebug() << "Aucun cas prévu";
    }
-   jobject["data"] = QString::number(25);
+
    jobject["status"] = "Connected";
    jobject["id"] = "42";
    this->publishData(topic, jobject);
-
 }
