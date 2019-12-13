473787fa031c91f41fba2112bbaa9bcb6211a355
Joris Offouga <offougajoris@gmail.com>
2019-12-13 16:09:32 +0100

    gateway: Remove unused include and define
    Signed-off-by: Joris Offouga <offougajoris@gmail.com>

--- gateway/gateway.h
+++ gateway/gateway.h
@@ -1,18 +1,8 @@
 #ifndef MQTTHERIT_H
 #define MQTTHERIT_H
 
-#include <QObject>
-#include <QtMqtt/QMqttClient>
-#include <QString>
-#include <QJsonArray>
-#include <QJsonDocument>
-#include <QJsonObject>
-#include <QList>
-#include <QDebug>
 #include "mqtthandler.h"
 
-#define MQTT_ADDR "192.168.0.1"
-#define MQTT_PORT 1883
 #define MQTT_SENSOR_TOPIC "/sensor/#"
 #define MQTT_GATEWAY_TOPIC "/gateway/#"
 
